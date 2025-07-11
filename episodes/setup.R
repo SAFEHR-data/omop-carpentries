d3e4d6a2e0f96ad8edce8b59844501464d636557
Sarah Keating <s.keating@ucl.ac.uk>
2025-07-11 09:54:05 +0100

    see if this works for not needing to declare omopcept

---
+++ episodes/setup.R
@@ -0,0 +1,15 @@
+install.packages("dplyr")
+
+install.packages("readr")
+
+install.packages("purrr")
+
+install.packages("CDMConnector")
+
+install.packages("duckdb")
+
+install.packages("remotes")
+
+remotes::install_github("SAFEHR-data/omopcept", force = TRUE)
+
+library(omopcept)
