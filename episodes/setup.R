a17b78118df653106ee33e7b69ea8acdc479b16d
Sarah Keating <s.keating@ucl.ac.uk>
2025-07-13 12:02:48 +0100

    TRYING

---
+++ episodes/setup.R
@@ -0,0 +1,23 @@
+install.packages("dplyr")
+
+install.packages("readr")
+
+install.packages("purrr")
+
+install.packages("CDMConnector")
+
+install.packages("duckdb")
+
+install.packages("lubridate")
+
+install.packages("renv")
+
+install.packages("remotes")
+
+remotes::install_github("SAFEHR-data/omopcept", force = TRUE)
+
+renv::activate()
+
+renv::restore()
+
+library(omopcept)
