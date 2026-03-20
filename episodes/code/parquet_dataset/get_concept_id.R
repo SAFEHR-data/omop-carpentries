d32255b690c9bb500938589b8aa3f8c961e15c36
Stef Piatek <s.piatek@ucl.ac.uk>
2026-03-20 18:41:49 +0000

    Update provided helper functions to use public directory directly

--- episodes/code/parquet_dataset/get_concept_id.R
+++ episodes/code/parquet_dataset/get_concept_id.R
@@ -1,5 +1,5 @@
 get_concept_id <- function(omop_obj, name) {
-  omop_obj$public$concept |>
+  omop_obj$concept |>
     filter(concept_name == !!name) |>
     select(concept_id) |>
     collect()
--- episodes/code/parquet_dataset/get_concept_name.R
+++ episodes/code/parquet_dataset/get_concept_name.R
@@ -1,5 +1,5 @@
 get_concept_name <- function(omop_obj, id) {
-  omop_obj$public$concept |>
+  omop_obj$concept |>
     filter(concept_id == !!id) |>
     select(concept_name) |>
     collect()
