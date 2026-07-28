df0ce2e35a531012e95b533848e2c8510cecabaa
Stef Piatek <s.piatek@ucl.ac.uk>
2026-07-28 14:05:56 +0100

    Revert "Convert doubles to integers for concepts"
    This reverts commit 095a8fa4662da45487bbd33f8fa9b09a5b0a7c16.

--- instructors/files/convert_parquet_file_to_dataset.R
+++ instructors/files/convert_parquet_file_to_dataset.R
@@ -5,21 +5,16 @@ library(lubridate)
 
 original_files <- list.files(path=here("episodes/data/omop"), pattern = "*", full.names=TRUE)
 
-#' Convert columns to the correct data types and write as dataset
-#' In case the data has been written as a parquet file, where these will be
-#' - strings for date and timestamps
-#' - double for integer fields
+#' Parse date and datetime columns to the correct data types and write as dataset
+#' In case the data has been written as a parquet file, which converts these to strings
 convert_dataset_column_types <- function(filepath) {
   print(glue::glue("Processing {filepath}"))
   open_dataset(filepath) |>
     mutate(
       across(ends_with("_date"), ~ lubridate::dmy(.x)),
-      across(ends_with("_datetime"), ~ lubridate::dmy_hm(.x)),
-      across(ends_with("_id") & where(is.double), ~ as.integer(.x))
-    ) |>
+      across(ends_with("_datetime"), ~ lubridate::dmy_hm(.x))
+      ) |>
     write_dataset(path = filepath, format = "parquet", create_directory = FALSE)
 }
 
 purrr::walk(original_files, convert_dataset_column_types)
-
-
